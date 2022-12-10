ff69130b47c024a8b4362d6570281e2eefc08325
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-10 22:12:07 +0530

    Update classic.py

--- Alpha/classic.py
+++ Alpha/classic.py
@@ -1,7 +1,6 @@
 from pyrogram import Client, filters, enums
-from . import hl, on9_id, PLAYING, ews
+from . import hl, on9_id, PLAYING, get_classic_word
 
-BIN = {}
 @Client.on_message(filters.command("classic", hl) & filters.me)
 async def classic(_, m):
     global PLAYING
