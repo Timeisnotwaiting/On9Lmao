7fc46d0ef1956d04bf86a5804ce477f9e967b2b0
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-10 22:05:23 +0530

    Update classic.py

--- Alpha/classic.py
+++ Alpha/classic.py
@@ -1,7 +1,10 @@
 from pyrogram import Client, filters, enums
-from . import hl, on9_id
+from . import hl, on9_id, PLAYING
 
 @Client.on_message(filters.command("classic", hl) & filters.me)
 async def classic(_, m):
+    global PLAYING
+    if PLAYING:
+        return await m.edit("***ALREADY IN GAME***")
     
     
