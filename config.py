e9a39c96c345b24af8f9da96d428f24af2f8bb91
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-10 22:01:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,8 @@ from os import environ as e
 API_ID = int(e["API_ID"])
 API_HASH = e["API_HASH"]
 STRING_SESSION = e["STRING_SESSION"]
+COMMAND_HANDLER = e["COMMAND_HANDLER"]
+
+# don't change this ! 😒😒
+
+on9_id = 840338206
