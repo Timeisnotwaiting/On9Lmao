7b5bf4e077d5fc387a31070d28e14511023d9bfd
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-11 00:18:46 +0530

    Update classic.py

--- Alpha/classic.py
+++ Alpha/classic.py
@@ -10,3 +10,11 @@ async def classic(_, m):
         return await m.edit("***ALREADY IN GAME***")
     PLAYING = True
     return await m.edit("***STARTED***")
+
+@Client.on_message(filters.command(["end", "stop", "terminate"], hl) & filters.me)
+async def endf(_, m):
+    global PLAYING
+    if not PLAYING:
+        return await m.edit("***NO GAME TO TERMINATE***")
+    PLAYING = False
+    return await m.edit("***TERMINATED***")
