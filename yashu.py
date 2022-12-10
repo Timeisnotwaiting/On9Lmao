c4f6ee296d8cee4ba90384e296c6d1145725cf6d
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-10 23:55:05 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, idle
 from config import *
 
-yashu = Client(":Alpha-Op:", api_id=API_ID, api_hash=API_HASH, string_session=STRING_SESSION, plugins=dict(root="Alpha"))
+yashu = Client(":Alpha-Op:", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION, plugins=dict(root="Alpha"))
 
 yashu.start()
 x = (yashu.get_me()).username
