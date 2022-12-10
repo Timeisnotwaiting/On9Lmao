464cb6aa69246ff84e957626cfa9059482ff49de
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-10 23:17:48 +0530

    Update and rename alpha.py to yashu.py

--- yashu.py
+++ yashu.py
@@ -4,5 +4,6 @@ from config import *
 yashu = Client(":Alpha-Op:", api_id=API_ID, api_hash=API_HASH, string_session=STRING_SESSION, plugins=dict(root="Alpha"))
 
 yashu.start()
-print("bot started !")
+x = (yashu.get_me()).username
+print(f"@{x} started \n\nTHE ALPHA !")
 idle()
