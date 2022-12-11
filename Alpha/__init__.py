eafb88690ea6b0580d78f565a22e4f414d4847da
Keshava Tripathi <110448220+NotKeshav@users.noreply.github.com>
2022-12-11 21:45:55 +0530

    Update __init__.py

--- Alpha/__init__.py
+++ Alpha/__init__.py
@@ -1,13 +1,8 @@
 from config import COMMAND_HANDLER as hl, on9_id
 from english_words import english_words_set as ews
 
-PLAYING = False
 
-PLAYERS = []
-
-BIN = []
-def get_classic_word(letter, length):
-    global BIN
+def get_classic_word(letter, length, BIN):
     for x in ews:
         if x in BIN:
             continue
